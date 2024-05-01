3212aaa1a6224ba658d82a7ac31b459d9e444c32
cesar <cesar.moreno04@uptc.edu.co>
2024-04-30 22:39:26 -0500

    Configure folder structure

---
+++ Scripts/Player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const moveSpeed = 70
+const maxSpeed = 100
+const jumpHeight = -300
+const gravity = 15
+
+@onready var sprite = $Sprite2D
+@onready var animationPlayer = $AnimationPlayer
+
+
+
+func _physics_process(_delta):
+	velocity.y += gravity
+	var friction = false
+
+	if Input.is_action_pressed("ui_right"):
+		sprite.flip_h = true
+		animationPlayer.play("Walk")
+		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
+
+	elif Input.is_action_pressed("ui_left"):
+		sprite.flip_h = false
+		animationPlayer.play("Walk")
+		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
+
+	else:
+		animationPlayer.play("Idle")
+		friction = true
+
+	if is_on_floor():
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = jumpHeight
+		if friction:
+			velocity.x = lerp(velocity.x, 0.0, 0.5)
+	else:
+		if friction:
+			velocity.x = lerp(velocity.x, 0.0, 0.01)
+
+	move_and_slide()
