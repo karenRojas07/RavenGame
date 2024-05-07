de38eb1e0bfdf67a43085dfbbcf6a3a246a7bd87
cesar <cesar.moreno04@uptc.edu.co>
2024-05-07 17:38:02 -0500

    Add animation for the character

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,27 +9,26 @@ const gravity = 15
 @onready var animationPlayer = $AnimationPlayer
 
 
-
 func _physics_process(_delta):
 	velocity.y += gravity
 	var friction = false
 
 	if Input.is_action_pressed("ui_right"):
-		sprite.flip_h = true
-		animationPlayer.play("Walk")
+		sprite.flip_h = false
+		animationPlayer.play("WALK")
 		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
 
 	elif Input.is_action_pressed("ui_left"):
-		sprite.flip_h = false
-		animationPlayer.play("Walk")
+		sprite.flip_h = true
+		animationPlayer.play("WALK")
 		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
 
 	else:
-		animationPlayer.play("Idle")
+		animationPlayer.play("IDLE")
 		friction = true
 
 	if is_on_floor():
-		if Input.is_action_pressed("ui_accept"):
+		if Input.is_action_pressed("ui_up"):
 			velocity.y = jumpHeight
 		if friction:
 			velocity.x = lerp(velocity.x, 0.0, 0.5)
