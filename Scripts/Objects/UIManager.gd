6840c592fa6a8aa08926218dadd7f88124c43c20
cesar <cesar.moreno04@uptc.edu.co>
2024-05-25 21:58:02 -0500

    Set change of levels

--- Scripts/Objects/UIManager.gd
+++ Scripts/Objects/UIManager.gd
@@ -4,8 +4,11 @@ extends CanvasLayer
 
 func _ready():
 	if GameManager.player:
-		GameManager.player.connect("health_changed", Callable(self, "update_heart_display"))
-	update_heart_display(GameManager.player.health, GameManager.player.max_health)
+		if !GameManager.player.is_queued_for_deletion():
+			GameManager.player.connect("health_changed", Callable(self, "update_heart_display"))
+			update_heart_display(GameManager.player.health, GameManager.player.max_health)
 
 func update_heart_display(current_health, max_health):
-	heart_counter.text = str(current_health) + " / " + str(max_health)
+	if GameManager.player:
+		if !GameManager.player.is_queued_for_deletion():
+			heart_counter.text = str(current_health) + " / " + str(max_health)
--- Scripts/Objects/Varios/door.gd
+++ Scripts/Objects/Varios/door.gd
@@ -3,17 +3,16 @@ extends Node2D
 var llave_recogida: bool = false
 
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
-
 
 func _on_area_2d_area_entered(area):
 	if area.get_parent() is Player:
 		if area.get_parent().llave_recogida:
 			$AnimationPlayer.play("open")
 			area.get_parent().delete_key()
+			await get_tree().create_timer(2.0).timeout
+			get_tree().change_scene_to_file("res://Scenes/Map2.tscn")
+			area.get_parent().delete_key()
+			
+
 
